d2fc1ed89856a1eb1fce876fa5eb95ded21b9255
Aiko Klostermann <aiko@open.gov.sg>
2023-05-03 12:28:22 +0800

    feat: add findAll templates endpoint

--- backend/src/templates/templates.service.ts
+++ backend/src/templates/templates.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Template } from 'database/entities'
+import { Repository } from 'typeorm'
 
 import { CreateTemplateDto } from '~shared/dtos/create-template.dto'
 import { UpdateTemplateDto } from '~shared/dtos/update-template.dto'
 
 @Injectable()
 export class TemplatesService {
+  constructor(
+    @InjectRepository(Template)
+    private readonly repository: Repository<Template>,
+  ) {}
+
   create(createTemplateDto: CreateTemplateDto) {
     return 'This action adds a new template'
   }
 
-  findAll() {
-    return `This action returns all templates`
+  async findAll() {
+    return await this.repository.find({
+      order: { createdAt: 'DESC' },
+    })
   }
 
   findOne(id: number) {
