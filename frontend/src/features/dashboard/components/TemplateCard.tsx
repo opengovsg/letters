e457a3353ddea8b46c353d9c840ef23290534756
Aiko Klostermann <aiko@open.gov.sg>
2023-05-04 17:38:02 +0800

    feat: include day.js to format time since edit in template card

--- frontend/src/features/dashboard/components/TemplateCard.tsx
+++ frontend/src/features/dashboard/components/TemplateCard.tsx
@@ -1,21 +1,30 @@
-import { Card, CardHeader, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardHeader, Flex, Heading, Image, Text } from '@chakra-ui/react'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
 
 type TemplateCardProps = {
-	name: string;
-	updatedAt: string;
-	thumbnailS3Path: string;
-};
+  name: string
+  updatedAt: string
+  thumbnailS3Path: string
+}
 
 export const TemplateCard = (templateCardProps: TemplateCardProps) => {
-	return (
-		<Card padding={5} width={240} height={330}>
-			<Flex justifyContent="center">
-				<Image src="./template.png" width={170} height={170} />
-			</Flex>
-			<CardHeader>
-				<Heading size="20px">{templateCardProps.name}</Heading>
-				<Text fontSize="xs">Edited on {templateCardProps.updatedAt}</Text>
-			</CardHeader>
-		</Card>
-	);
-};
+  return (
+    <Card padding={5} width={240} height={330}>
+      <Flex justifyContent="center">
+        <Image
+          src={templateCardProps.thumbnailS3Path}
+          width={170}
+          height={170}
+        />
+      </Flex>
+      <CardHeader>
+        <Heading size="20px">{templateCardProps.name}</Heading>
+        <Text fontSize="xs">
+          Edited {dayjs(templateCardProps.updatedAt).fromNow()}
+        </Text>
+      </CardHeader>
+    </Card>
+  )
+}
