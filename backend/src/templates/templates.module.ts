aee8510aded6b0d15411469b7031f90a858e8473
Aiko Klostermann <aiko@open.gov.sg>
2023-05-04 10:35:01 +0800

    feat: remove templates unit tests as was decided to go without initially

--- backend/src/templates/templates.module.ts
+++ backend/src/templates/templates.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Template } from 'database/entities'
 
 import { TemplatesController } from './templates.controller'
 import { TemplatesService } from './templates.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Template])],
   controllers: [TemplatesController],
   providers: [TemplatesService],
 })
