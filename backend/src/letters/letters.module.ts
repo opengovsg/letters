f83d71fa9f2b4ca443b3e51bedfcbedde2bdaf3b
disKeith <119388168+disKeith@users.noreply.github.com>
2023-05-05 14:05:27 +0800

    feat: handle bulk request and json validation

--- backend/src/letters/letters.module.ts
+++ backend/src/letters/letters.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { Template } from '../database/entities' // To be deleted
+import { TemplatesService } from '../templates/templates.service'
 import { LettersController } from './letters.controller'
 import { LettersService } from './letters.service'
+import { ValidationService } from './letters-validation.service'
 
 @Module({
   controllers: [LettersController],
-  providers: [LettersService],
+  providers: [LettersService, ValidationService, TemplatesService],
+  imports: [TypeOrmModule.forFeature([Template])], // To be deleted
 })
 export class LettersModule {}
---
+++ shared/src/constants/letters.ts
@@ -0,0 +1,1 @@
+export const BULK_MAX_ROW_LENGTH = 10
