44f80cce50665b75616e02398dd277332cd6ce40
Aiko Klostermann <aiko@open.gov.sg>
2023-05-05 17:58:47 +0800

    feat: add hover to zoom and show issue letter button on template card

--- frontend/src/features/dashboard/components/TemplateCard.tsx
+++ frontend/src/features/dashboard/components/TemplateCard.tsx
@@ -1,4 +1,12 @@
-import { Card, CardHeader, Flex, Heading, Image } from '@chakra-ui/react'
+import {
+  Button,
+  Card,
+  CardHeader,
+  Flex,
+  Heading,
+  Image,
+} from '@chakra-ui/react'
+import React from 'react'
 
 type TemplateCardProps = {
   name: string
@@ -6,8 +14,32 @@ type TemplateCardProps = {
 }
 
 export const TemplateCard = (templateCardProps: TemplateCardProps) => {
+  const [zoom, setZoom] = React.useState(false)
+
+  function handleMouseHover() {
+    setZoom((prev) => !prev)
+  }
+
   return (
-    <Card padding={5} width={240} height={260}>
+    // WIP:
+    // Issues: - parent container does not increase when hover/zoom -> card side cut off
+    //         - Button and Card size magically change/appear on hover/zoom -> should be a transition
+    <Card
+      style={
+        zoom
+          ? {
+              transition: 'transform .2s',
+              transform: 'scale(1.2)',
+              zIndex: '1',
+            }
+          : {}
+      }
+      padding={5}
+      width={240}
+      height={zoom ? 330 : 260}
+      onMouseEnter={handleMouseHover}
+      onMouseLeave={handleMouseHover}
+    >
       <Flex justifyContent="center">
         <Image
           src={templateCardProps.thumbnailS3Path}
@@ -18,6 +50,7 @@ export const TemplateCard = (templateCardProps: TemplateCardProps) => {
       <CardHeader>
         <Heading size="20px">{templateCardProps.name}</Heading>
       </CardHeader>
+      {zoom && <Button>Issue Letter</Button>}
     </Card>
   )
 }
