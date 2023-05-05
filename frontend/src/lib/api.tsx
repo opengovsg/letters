76415bb2978c22912d6f5d53d298fc9415e732ee
Aiko Klostermann <aiko@open.gov.sg>
2023-05-05 09:55:51 +0800

    feat: fix lint

--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -1,20 +1,20 @@
-import wretch from "wretch";
+import wretch from 'wretch'
 
-import { UNAUTHORIZED_EVENT } from "~constants/events";
+import { UNAUTHORIZED_EVENT } from '~constants/events'
 
 /**
  * Default API client pointing to backend.
  * Automatically catches 403 errors and invalidates authentication state.
  */
-export const api = wretch("/api/v1")
-	.catcher(403, (err) => {
-		window.dispatchEvent(new Event(UNAUTHORIZED_EVENT));
-		throw err;
-	})
-	.errorType("json");
+export const api = wretch('/api/v1')
+  .catcher(403, (err) => {
+    window.dispatchEvent(new Event(UNAUTHORIZED_EVENT))
+    throw err
+  })
+  .errorType('json')
 
 //Leaving this here since our endpoints are currently not prefixed with 'api/v1/'
-export const unversionedApi = wretch("").catcher(403, (err) => {
-	window.dispatchEvent(new Event(UNAUTHORIZED_EVENT));
-	throw err;
-});
+export const unversionedApi = wretch('').catcher(403, (err) => {
+  window.dispatchEvent(new Event(UNAUTHORIZED_EVENT))
+  throw err
+})
