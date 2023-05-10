19c56c3dd75edf8c9472cce679353e1aac45bee1
Nicholas Ong <nicholasongck@gmail.com>
2023-05-10 15:41:30 +0800

    chore: prettier refactor

--- frontend/src/layouts/PublicLayout/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout/PublicLayout.tsx
@@ -7,30 +7,30 @@ import { routes } from "~constants/routes";
 // import LogoSvg from '~/assets/Logo.svg'
 
 export const PublicLayout = () => {
-	return (
-		<VStack minWidth="100%" align="stretch" spacing={0}>
-			<Flex
-				position="static"
-				pos="relative"
-				flexDir="row"
-				p={6}
-				justifyContent="space-between"
-				borderBottom="1px"
-				borderBottomColor="base.divider.medium"
-				w="full"
-				align="center"
-			>
-				{/* <Image maxW="8rem" src={LogoSvg} /> */}
-				<RouterLink to={routes.index}>
-					{/* <Image maxW="8rem" src={LogoSvg} ml={'8px'} /> */}
-					Letters
-				</RouterLink>
-			</Flex>
+  return (
+    <VStack minWidth="100%" align="stretch" spacing={0}>
+      <Flex
+        position="static"
+        pos="relative"
+        flexDir="row"
+        p={6}
+        justifyContent="space-between"
+        borderBottom="1px"
+        borderBottomColor="base.divider.medium"
+        w="full"
+        align="center"
+      >
+        {/* <Image maxW="8rem" src={LogoSvg} /> */}
+        <RouterLink to={routes.index}>
+          {/* <Image maxW="8rem" src={LogoSvg} ml={'8px'} /> */}
+          Letters
+        </RouterLink>
+      </Flex>
 
-			<Box minH="admin-content-min-height" w="full" bg="gray.100">
-				<Outlet />
-			</Box>
-			<AppFooter />
-		</VStack>
-	);
+      <Box minH="admin-content-min-height" w="full" bg="gray.100">
+        <Outlet />
+      </Box>
+      <AppFooter />
+    </VStack>
+  );
 };
