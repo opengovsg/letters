8c6248af5cac493d3baa379453fff64f395b95e6
Alexis <gweiying@gmail.com>
2023-05-12 11:39:02 +0800

    chore: fix single render return type

--- backend/src/letters/letters-rendering.service.ts
+++ backend/src/letters/letters-rendering.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@nestjs/common'
 
+export class RenderedLetter {
+  issuedLetter: string
+  fieldValues: string
+}
+
 @Injectable()
 export class LettersRenderingService {
   readonly PLACE_HOLDER_PREFIX = '{{'
   readonly PLACE_HOLDER_SUFFIX = '}}'
 
-  render(html: string, letterParamMap: { [key: string]: string }): string {
+  render(
+    html: string,
+    letterParamMap: { [key: string]: string },
+  ): RenderedLetter {
     for (const key in letterParamMap) {
       const value = letterParamMap[key]
       const placeHolder = `${this.PLACE_HOLDER_PREFIX}${key}${this.PLACE_HOLDER_SUFFIX}`
       html = html.replace(placeHolder, value)
     }
-    return html
+    return { issuedLetter: html, fieldValues: JSON.stringify(letterParamMap) }
   }
 
-  bulkRender(html: string, letterParamMaps: { [key: string]: string }[]) {
-    return letterParamMaps.map((letterParamMap) => ({
-      issuedLetter: this.render(html, letterParamMap),
-      fieldValues: JSON.stringify(letterParamMap),
-    }))
+  bulkRender(
+    html: string,
+    letterParamMaps: { [key: string]: string }[],
+  ): RenderedLetter[] {
+    return letterParamMaps.map((letterParamMap) =>
+      this.render(html, letterParamMap),
+    )
   }
 }
