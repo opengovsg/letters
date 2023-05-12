d2bd0ba5e64c47958e55ca09e615b6dea15aa803
Alexis <gweiying@gmail.com>
2023-05-12 17:57:11 +0800

    chore: refactor tinymce editor components to use context

--- frontend/src/features/public/LetterPublicPage.tsx
+++ frontend/src/features/public/LetterPublicPage.tsx
@@ -2,8 +2,8 @@ import { VStack } from '@chakra-ui/react'
 import { Navigate } from 'react-router-dom'
 
 import { routes } from '~constants/routes'
+import { Editor } from '~features/tinymce/components/Editor'
 
-import { LetterViewer } from './components/LetterViewer'
 import {
   useGetLetterByPublicId,
   useLetterPublicId,
@@ -21,9 +21,11 @@ export const LetterPublicPage = (): JSX.Element => {
   return (
     <VStack alignItems="left" spacing="0px">
       <VStack padding={16} spacing={8} align={'center'}>
-        <LetterViewer
-          htmlLetter={letter?.issuedLetter}
+        <Editor
+          html={letter?.issuedLetter}
           isLoading={isLetterLoading}
+          isDisabled={true}
+          isInline={true}
         />
       </VStack>
     </VStack>
---
+++ frontend/src/features/tinymce/components/Editor.tsx
@@ -0,0 +1,44 @@
+import { Box, Spinner } from '@chakra-ui/react'
+import { Editor as TinymceReactEditor } from '@tinymce/tinymce-react'
+
+import {
+  TinymceProvider,
+  useTinymce,
+} from '~features/tinymce/context/TinymceProvider'
+
+interface LetterViewerProps {
+  html: string | undefined
+  isLoading: boolean
+  isDisabled?: boolean
+  isInline?: boolean
+}
+
+const TinymceEditor = ({
+  html,
+  isLoading,
+  isDisabled = false,
+  isInline = false,
+}: LetterViewerProps): JSX.Element => {
+  const { tinymceApiKey } = useTinymce()
+  if (isLoading) {
+    return <Spinner />
+  }
+  return (
+    <Box border="1px" borderColor="grey.200" p={8} bg="white">
+      <TinymceReactEditor
+        apiKey={tinymceApiKey}
+        initialValue={html}
+        init={{
+          inline: isInline,
+        }}
+        disabled={isDisabled}
+      />
+    </Box>
+  )
+}
+
+export const Editor = (props: JSX.IntrinsicAttributes & LetterViewerProps) => (
+  <TinymceProvider>
+    <TinymceEditor {...props} />
+  </TinymceProvider>
+)
