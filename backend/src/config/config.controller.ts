adebae61d16c67b11bc1434cd3ed6b3d701f0da3
Alexis <gweiying@gmail.com>
2023-05-15 16:25:34 +0800

    chore: remove unused auth guard

--- backend/src/config/config.controller.ts
+++ backend/src/config/config.controller.ts
@@ -1,15 +1,7 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Param,
-  UseGuards,
-} from '@nestjs/common'
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
 
-// import { AuthGuard } from '../auth/auth.guard'
 import { ConfigService } from './config.service'
 
-// @UseGuards(AuthGuard)
 @Controller('config')
 export class ConfigController {
   constructor(private readonly configService: ConfigService) {}
