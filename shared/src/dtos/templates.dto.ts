e852f65989d4d258b0f206839269593c76a01771
Nicholas Ong <nicholasongck@gmail.com>
2023-05-15 11:05:47 +0800

    fix: change class validation

--- shared/src/dtos/templates.dto.ts
+++ shared/src/dtos/templates.dto.ts
@@ -1,8 +1,8 @@
-import { IsDefined, IsString } from 'class-validator'
+import { IsArray, IsDefined, IsString } from 'class-validator'
 
 export class CreateTemplateDto {
   @IsDefined()
-  @IsString()
+  @IsArray()
   fields: string[]
   @IsDefined()
   @IsString()
@@ -25,4 +25,4 @@ export class GetTemplateDto {
   updatedAt: string
 }
 
-export class UpdateTemplateDto {}
+export class UpdateTemplateDto { }
