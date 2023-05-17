c51b2e3bacde3f9c199c079219116f278ecd7591
gweiying <39231249+gweiying@users.noreply.github.com>
2023-05-17 17:53:51 +0800

    chore: introduce placeholder empty value on convict parser for env va… (#103)
    * chore: introduce placeholder empty value on convict parser for env variables
    
    * chore: update placeholder text

--- backend/src/config/config.service.ts
+++ backend/src/config/config.service.ts
@@ -5,6 +5,10 @@ import { CONFIG_ACCESSIBLE_BY_FRONTEND } from '~shared/constants/config'
 
 import { ConfigSchema, schema } from './config.schema'
 
+// AWS param store does not support empty values, so we use a string placeholder of `'VALUE_NOT_SET'`
+// If param store value is defined as `'VALUE_NOT_SET'`, we treat it as undefined and return the default value
+const PLACEHOLDER_UNDEFINED = 'VALUE_NOT_SET'
+
 @Injectable()
 export class ConfigService {
   config: Config<ConfigSchema>
@@ -17,6 +21,9 @@ export class ConfigService {
   // We want to implicitly use the return type of convict get method.
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   get<K extends Path<ConfigSchema>>(key: K) {
+    if (this.config.get(key) === PLACEHOLDER_UNDEFINED) {
+      return this.config.default(key)
+    }
     return this.config.get(key)
   }
 
