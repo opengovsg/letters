a177645d286a92f8ee5b36cd0fa4473a1c50f761
Alexis <gweiying@gmail.com>
2023-05-18 15:09:05 +0800

    chore: add assets to ErrorPage

--- frontend/src/features/public/ErrorPage.tsx
+++ frontend/src/features/public/ErrorPage.tsx
@@ -1,23 +1,26 @@
-import { Flex, Image, Spacer, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Image, Spacer, Text, VStack } from '@chakra-ui/react'
+
+import NotFoundSvg from '~/assets/NotFound.svg'
 
 export const ErrorPage = (): JSX.Element => {
   return (
-    <Flex
-      flexDir={{ base: 'column', md: 'row' }}
-      maxW={{ base: '100vw', md: '1080px' }}
-      alignSelf="center"
-      align="center"
-      justifySelf="center"
-      p={{ base: 6, md: 10 }}
-    >
-      <Spacer />
-      <VStack align="stretch" spacing={8} maxW={{ base: 'unset', md: '360px' }}>
-        <Text>This letters link is not available.</Text>
-        <Text>
-          Please double check your letters link or contact the agency who has
-          given you this link.
-        </Text>
-      </VStack>
+    <Flex pt={8} justify="center" minHeight="100vh">
+      <Box maxW="60%">
+        <Flex
+          direction={{ base: 'column', md: 'row' }}
+          align="center"
+          justify="center"
+        >
+          <Image src={NotFoundSvg} aria-hidden w={{ base: '80%', md: '40%' }} />
+          <VStack align="stretch" spacing={8} ml={{ base: 0, md: 8 }}>
+            <Text textAlign="center">This link is not available.</Text>
+            <Text textAlign="center">
+              Please double check your link or contact the agency who has given
+              you this link.
+            </Text>
+          </VStack>
+        </Flex>
+      </Box>
     </Flex>
   )
 }
