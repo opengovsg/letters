a4a9886c389260781df11b454fae50128c993acd
Alexis <gweiying@gmail.com>
2023-05-18 17:26:21 +0800

    chore: remove unnecessary typing

--- shared/src/constants/links.ts
+++ shared/src/constants/links.ts
@@ -1,6 +1,6 @@
 export const CONTACT_US = 'https://go.gov.sg/letters-user-support'
 export const USER_GUIDE = 'https://go.gov.sg/letters-user-guide'
-export const BETA_SIGNUP = 'https://go.gov.sg/letters-beta' as string
+export const BETA_SIGNUP = 'https://go.gov.sg/letters-beta'
 export const TERMS_OF_USE = 'https://hack.gov.sg/2023/HFPGTC'
 export const REPORT_VULNERABILITY =
   'https://www.tech.gov.sg/report_vulnerability'
