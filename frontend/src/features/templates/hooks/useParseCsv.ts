3ed9c03cf0d56432435ba0479ff5ed4dc0a333ff
Alexis <gweiying@gmail.com>
2023-05-23 15:08:35 +0800

    chore: add useParseCsv hook

---
+++ frontend/src/features/templates/hooks/useParseCsv.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+import { csvToJsonArr } from '~utils/csvUtils'
+
+const useParseCsv = () => {
+  const [parsedArr, setParsedArr] = useState<Array<{ [key: string]: string }>>(
+    [],
+  )
+  const [error, setError] = useState<string>('')
+
+  const parseCsv = async (file?: File | undefined): Promise<void> => {
+    if (!file) return
+    setError('') // reset error
+    try {
+      const parsedData = await csvToJsonArr(file)
+      if (parsedData.length === 0) {
+        setError('CSV does not contain any rows, please upload an updated .csv')
+      }
+      setParsedArr(parsedData as Array<{ [key: string]: string }>)
+    } catch (error) {
+      setError('Error parsing CSV file')
+    }
+  }
+
+  return {
+    parsedArr,
+    error,
+    parseCsv,
+  }
+}
+
+export default useParseCsv
