2dbc829322fdd7b32026311697af82f31b151dba
disKeith <119388168+disKeith@users.noreply.github.com>
2023-06-13 19:26:35 +0800

    feat: shared html and field validation util

--- shared/src/util/templates.ts
+++ shared/src/util/templates.ts
@@ -1,4 +1,7 @@
-import { TEMPLATE_KEYWORD_REGEX } from '../constants/regex'
+import {
+  TEMPLATE_KEYWORD_CHAR_REGEX,
+  TEMPLATE_KEYWORD_REGEX,
+} from '../constants/regex'
 
 export const convertFieldsToLowerCase = (fields: string[]): string[] =>
   fields.map((field: string) => field.toLowerCase())
@@ -7,8 +10,32 @@ export const deduplicateFields = (fields: string[]): string[] => [
   ...new Set(fields),
 ]
 
+export const isFieldsInvalid = (fields: string[]) => {
+  const invalidFields: string[] = []
+
+  fields.forEach((field) => {
+    if (!TEMPLATE_KEYWORD_CHAR_REGEX.test(field)) invalidFields.push(field)
+  })
+
+  return invalidFields.length > 0 ? invalidFields : false
+}
+
 export const setHtmlKeywordsToLowerCase = (html: string): string =>
   html.replace(
     TEMPLATE_KEYWORD_REGEX,
     (match: string, keyword: string) => `{{${keyword.toLowerCase()}}}`,
   )
+
+export const isHtmlKeywordsInvalid = (html: string) => {
+  const invalidFields: string[] = []
+  const matches = html.match(TEMPLATE_KEYWORD_REGEX)
+
+  if (matches) {
+    matches.forEach((match) => {
+      const key = match.slice(2, -2)
+      if (!TEMPLATE_KEYWORD_CHAR_REGEX.test(key)) invalidFields.push(match)
+    })
+  }
+
+  return invalidFields.length > 0 ? invalidFields : false
+}
