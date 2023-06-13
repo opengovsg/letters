20c4e00b5ba64c7c17a7bb618e94223390230971
disKeith <119388168+disKeith@users.noreply.github.com>
2023-06-13 19:28:33 +0800

    feat: backend html and fields validation

--- backend/src/templates/templates-parsing.service.ts
+++ backend/src/templates/templates-parsing.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 
 import { CreateTemplateDto } from '~shared/dtos/templates.dto'
 import { sanitizeHtml } from '~shared/util/html-sanitizer'
 import {
   convertFieldsToLowerCase,
   deduplicateFields,
+  isFieldsInvalid,
+  isHtmlKeywordsInvalid,
   setHtmlKeywordsToLowerCase,
 } from '~shared/util/templates'
 
@@ -12,10 +14,22 @@ import {
 export class TemplatesParsingService {
   processTemplate(createTemplateDto: CreateTemplateDto) {
     const sanitizedHtml = sanitizeHtml(createTemplateDto.html)
+    createTemplateDto.html = setHtmlKeywordsToLowerCase(sanitizedHtml)
     createTemplateDto.fields = deduplicateFields(
       convertFieldsToLowerCase(createTemplateDto.fields),
     )
-    createTemplateDto.html = setHtmlKeywordsToLowerCase(sanitizedHtml)
+
+    const invalidHtml = isHtmlKeywordsInvalid(createTemplateDto.html)
+    if (invalidHtml)
+      throw new BadRequestException(
+        `Invalid html fields: ${invalidHtml.join(', ')}`,
+      )
+
+    const invalidFields = isFieldsInvalid(createTemplateDto.fields)
+    if (invalidFields)
+      throw new BadRequestException(
+        `Invalid fields: ${invalidFields.join(', ')}`,
+      )
 
     return createTemplateDto
   }
