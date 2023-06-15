3a9059a5516fb66d46a3272c48c1676b4f475e24
gweiying <39231249+gweiying@users.noreply.github.com>
2023-06-15 08:41:47 +0800

    Chore/search blocking (#152)
    * chore: install react-helmet
    
    * chore: add noindex tag to letters page

--- frontend/src/features/public/LetterPublicPage.tsx
+++ frontend/src/features/public/LetterPublicPage.tsx
@@ -1,4 +1,5 @@
 import { Button, VStack } from '@chakra-ui/react'
+import { Helmet } from 'react-helmet'
 import { Navigate } from 'react-router-dom'
 
 import { routes } from '~constants/routes'
@@ -26,17 +27,22 @@ export const LetterPublicPage = (): JSX.Element => {
     return <Navigate to={`/${routes.public.error}`} />
   }
   return (
-    <VStack alignItems="left" spacing="0px">
-      <VStack padding={16} spacing={8} align={'center'}>
-        <LetterViewer
-          letterPublicId={letterPublicId}
-          html={letter?.issuedLetter}
-          isLoading={isLetterLoading}
-          minWidth={{ md: WIDTH_A4 }}
-          minHeight={{ md: HEIGHT_A4 }}
-        />
-        <Button onClick={handleDownload}>Download as .PDF</Button>
+    <>
+      <Helmet>
+        <meta name="robots" content="noindex" data-react-helmet="true" />
+      </Helmet>
+      <VStack alignItems="left" spacing="0px">
+        <VStack padding={16} spacing={8} align={'center'}>
+          <LetterViewer
+            letterPublicId={letterPublicId}
+            html={letter?.issuedLetter}
+            isLoading={isLetterLoading}
+            minWidth={{ md: WIDTH_A4 }}
+            minHeight={{ md: HEIGHT_A4 }}
+          />
+          <Button onClick={handleDownload}>Download as .PDF</Button>
+        </VStack>
       </VStack>
-    </VStack>
+    </>
   )
 }
