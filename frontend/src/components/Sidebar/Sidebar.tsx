9ee332a5f1842728eebb3b8e58eb87fabd79e3a5
Aiko Klostermann <aiko@open.gov.sg>
2023-06-15 18:30:44 +0800

    chore: undo non-changes

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ export const Sidebar = ({ children }: { children: ReactNode }) => {
 
   return (
     <Box bg="gray.100" w="full">
-      <Flex align="stretch" w="full" minH="admin-content-min-height">
+      <Flex align="stretch" minH="admin-content-min-height" w="full">
         <Box
           bg="grey.50"
           borderRight="1px"
