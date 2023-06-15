cf0e016d189b6f4cd2d55578e3c962da36f1e20f
disKeith <119388168+disKeith@users.noreply.github.com>
2023-06-15 18:21:54 +0800

    chore: update regex and add shared html field retrieval function

--- shared/src/constants/regex.ts
+++ shared/src/constants/regex.ts
@@ -1,2 +1,3 @@
-export const TEMPLATE_KEYWORD_REGEX = /\{\{([^{}]+)\}\}/g
+export const TEMPLATE_KEYWORD_REGEX = /{{+(?:([a-z0-9_]+)}}+)/g
+export const TEMPLATE_KEYWORD_DELIMITER_REGEX = /\{\{([^{}]+)\}\}/g
 export const TEMPLATE_KEYWORD_CHAR_REGEX = /^[a-z0-9_]+$/
--- shared/src/util/templates.ts
+++ shared/src/util/templates.ts
@@ -1,5 +1,6 @@
 import {
   TEMPLATE_KEYWORD_CHAR_REGEX,
+  TEMPLATE_KEYWORD_DELIMITER_REGEX,
   TEMPLATE_KEYWORD_REGEX,
 } from '../constants/regex'
 
@@ -10,32 +11,33 @@ export const deduplicateFields = (fields: string[]): string[] => [
   ...new Set(fields),
 ]
 
+export const setHtmlKeywordsToLowerCase = (html: string): string => {
+  return html.replace(
+    TEMPLATE_KEYWORD_DELIMITER_REGEX,
+    (match: string) => `${match.toLowerCase()}`,
+  )
+}
+
+export const getHtmlFields = (html: string) => {
+  const fields: string[] = []
+  let match: RegExpExecArray | null
+
+  while ((match = TEMPLATE_KEYWORD_REGEX.exec(html)) !== null)
+    if (
+      TEMPLATE_KEYWORD_CHAR_REGEX.test(match[1]) &&
+      !fields.includes(match[1])
+    )
+      fields.push(match[1])
+
+  return convertFieldsToLowerCase(fields)
+}
+
 export const isFieldsInvalid = (fields: string[]) => {
   const invalidFields: string[] = []
 
   fields.forEach((field) => {
     if (!TEMPLATE_KEYWORD_CHAR_REGEX.test(field)) invalidFields.push(field)
   })
-
-  return invalidFields.length > 0 ? invalidFields : false
-}
-
-export const setHtmlKeywordsToLowerCase = (html: string): string =>
-  html.replace(
-    TEMPLATE_KEYWORD_REGEX,
-    (match: string, keyword: string) => `{{${keyword.toLowerCase()}}}`,
-  )
-
-export const isHtmlKeywordsInvalid = (html: string) => {
-  const invalidFields: string[] = []
-  const matches = html.match(TEMPLATE_KEYWORD_REGEX)
-
-  if (matches) {
-    matches.forEach((match) => {
-      const key = match.slice(2, -2)
-      if (!TEMPLATE_KEYWORD_CHAR_REGEX.test(key)) invalidFields.push(match)
-    })
-  }
 
   return invalidFields.length > 0 ? invalidFields : false
 }
