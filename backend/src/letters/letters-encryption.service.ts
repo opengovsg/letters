ac815246b7bfe5392a6eeec9094a8121051fa894
Aiko Klostermann <aiko@open.gov.sg>
2023-06-15 11:29:49 +0800

    chore: update postman collection to include password protected lettetrs

--- backend/src/letters/letters-encryption.service.ts
+++ backend/src/letters/letters-encryption.service.ts
@@ -1,14 +1,34 @@
-import { Injectable } from '@nestjs/common'
-import { AES } from 'crypto-js'
+import { Injectable, NotAcceptableException } from '@nestjs/common'
+import { AES, enc } from 'crypto-js'
+import { Letter } from 'database/entities'
 
 import { RenderedLetter } from './letters-rendering.service'
 
 @Injectable()
 export class LettersEncryptionService {
-  encryptLetters(letter: RenderedLetter, password: string): RenderedLetter {
+  encryptLetter(letter: RenderedLetter, password: string): RenderedLetter {
     return {
       fieldValues: AES.encrypt(letter.fieldValues, password).toString(),
       issuedLetter: AES.encrypt(letter.issuedLetter, password).toString(),
+    }
+  }
+
+  decryptLetter(letter: Letter, password: string): Letter {
+    const fieldValues = AES.decrypt(letter.fieldValues, password).toString(
+      enc.Utf8,
+    )
+    const issuedLetter = AES.decrypt(letter.issuedLetter, password).toString(
+      enc.Utf8,
+    )
+
+    if (fieldValues === '' && issuedLetter === '') {
+      throw new Error('Invalid Key')
+    }
+
+    return {
+      ...letter,
+      fieldValues,
+      issuedLetter,
     }
   }
 }
