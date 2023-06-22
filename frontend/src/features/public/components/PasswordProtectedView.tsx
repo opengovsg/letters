b60b1cde949d6ee990186d4b30276cf234d6ce79
Aiko Klostermann <12240377+AikoPath@users.noreply.github.com>
2023-06-22 15:09:17 +0800

    chore: add toggle to hide/show password (#159)

--- frontend/src/features/public/components/PasswordProtectedView.tsx
+++ frontend/src/features/public/components/PasswordProtectedView.tsx
@@ -7,10 +7,15 @@ import {
   GridProps,
   Image,
   Input,
+  InputGroup,
+  InputRightElement,
 } from '@chakra-ui/react'
-import { FC, FormEvent, PropsWithChildren } from 'react'
+import { FC, FormEvent, PropsWithChildren, useState } from 'react'
 
 import ELetters from '~/assets/ELetters.svg'
+import { ReactComponent as Hide } from '~/assets/Hide.svg'
+import { ReactComponent as Show } from '~/assets/Show.svg'
+import { ReactComponent as ShowEmpty } from '~/assets/ShowEmpty.svg'
 import { ResponseError } from '~/types/ResponseError'
 import { AppGrid } from '~templates/AppGrid'
 import { AuthGridArea } from '~templates/AuthGridArea'
@@ -48,6 +53,8 @@ export const PasswordProtectedView = ({
   setPassword,
   isLetterLoading,
 }: PasswordProtectedViewProps): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <BaseGridLayout flex={1} bg="white" h="100%">
       <NonMobileSidebarGridArea>
@@ -64,13 +71,26 @@ export const PasswordProtectedView = ({
               mb="2.5rem"
             >
               <FormLabel fontWeight={700}>Unlock Letter</FormLabel>
-              <Input
-                type="password"
-                placeholder="Enter password"
-                autoFocus
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
+              <InputGroup>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Enter password"
+                  autoFocus
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+                <InputRightElement width="3rem">
+                  <Box onClick={() => setShowPassword(!showPassword)}>
+                    {password.length === 0 ? (
+                      <ShowEmpty />
+                    ) : showPassword ? (
+                      <Hide />
+                    ) : (
+                      <Show />
+                    )}
+                  </Box>
+                </InputRightElement>
+              </InputGroup>
               <FormErrorMessage>{error?.json.message}</FormErrorMessage>
               <Button mt={4} w="100%" isLoading={isLetterLoading} type="submit">
                 Next
