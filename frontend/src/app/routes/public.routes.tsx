ad580073b2f9f160b64f99a68c8cc63e069b9c6a
Alexis <gweiying@gmail.com>
2023-06-23 16:11:22 +0800

    chore: remove unused catchall routes

--- frontend/src/app/routes/public.routes.tsx
+++ frontend/src/app/routes/public.routes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, RouteObject } from 'react-router-dom'
+import { RouteObject } from 'react-router-dom'
 
 import { PublicLayout } from '~/layouts/PublicLayout'
 import { routes } from '~constants/routes'
@@ -36,9 +36,5 @@ export const publicRoutes: RouteObject[] = [
         element: <ErrorPage />,
       },
     ],
-  },
-  {
-    path: '*',
-    element: <Navigate to={routes.public.error} />, // TODO: Differentiate between invalid letters link and invalid page?
   },
 ]
