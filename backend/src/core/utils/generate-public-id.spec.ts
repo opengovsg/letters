d9d167cacf7e95883c86580050a7102b1911a4a7
Alexis <gweiying@gmail.com>
2023-06-26 15:36:36 +0800

    chore: remove inline comments

--- backend/src/core/utils/generate-public-id.spec.ts
+++ backend/src/core/utils/generate-public-id.spec.ts
@@ -9,20 +9,15 @@ describe('generatePublicId', () => {
   })
 
   test('should regenerate ID if it matches a protected string', () => {
-    // Create a mock function that always returns a protected string
-    const mockGenerateId = jest
+    const mockGenerator = jest
       .fn()
       .mockReturnValueOnce('api')
       .mockReturnValue('abcdefghi')
 
-    // Replace the original generateId function with the mock function
-    const id = generatePublicId(mockGenerateId)
+    const id = generatePublicId(mockGenerator)
 
-    // Expect the mock generateId function to be called twice (once for the initial match and once for the regenerated ID)
-    expect(mockGenerateId).toHaveBeenCalledTimes(2)
-    // Expect the regenerated ID to be different from the protected string
+    expect(mockGenerator).toHaveBeenCalledTimes(2)
     expect(PROTECTED_NAMESPACES).not.toContain(id)
-    // Expect the output to be 'abcdefghi'
     expect(id).toEqual('abcdefghi')
   })
 })
