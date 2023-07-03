04f08b5c58ca34d968d73845b76747c320514921
PikkaPikkachu <prakriti_bansal@yahoo.com>
2023-07-03 12:11:14 +0800

    fix: csv uploads error UI (#167)

--- frontend/src/features/issue/BulkIssueDrawer/UploadCsvErrorsTable.tsx
+++ frontend/src/features/issue/BulkIssueDrawer/UploadCsvErrorsTable.tsx
@@ -42,6 +42,43 @@ export const UploadCsvErrorsTable = ({
     )
   }
 
+  const errorsByRow = uploadCsvErrors.reduce(
+    (errorGroup: Array<BulkLetterValidationResultError[]>, item) => {
+      if (errorGroup[item.id]) {
+        errorGroup[item.id].push(item)
+      } else {
+        errorGroup[item.id] = [item]
+      }
+      return errorGroup
+    },
+    new Array<BulkLetterValidationResultError[]>(uploadCsvErrors.length),
+  )
+
+  const errorTableRows = errorsByRow.map((errorData, index) => {
+    return (
+      <Tr key={JSON.stringify(index)}>
+        {/* We add 2 because the first row with data in the CSV is row 2, even though it has index 0 */}
+        <Td>{index + 2}</Td>
+        <Td>
+          {errorData.map((error, index) => {
+            return (
+              <Text key={index}>
+                {/* Add 1 because index starts from 0 actually */}
+                {index + 1}
+                {'. '}
+                {error.param}{' '}
+                {error.message ===
+                BulkLetterValidationResultErrorMessage.INVALID_ATTRIBUTE
+                  ? 'is not a valid attribute'
+                  : 'is missing'}
+              </Text>
+            )
+          })}
+        </Td>
+      </Tr>
+    )
+  })
+
   return (
     <Box padding={8}>
       <HStack>
@@ -65,23 +102,7 @@ export const UploadCsvErrorsTable = ({
             <HeaderCell>Row #</HeaderCell>
             <HeaderCell>Errors</HeaderCell>
           </Tr>
-          {uploadCsvErrors.map((error) => {
-            return (
-              // TODO: find a better key for the error
-              // TODO: group all errors of the same row number together. should this be done on the frontend or backend?
-              <Tr key={JSON.stringify(error)}>
-                {/* We add 2 because the first row with data in the CSV is row 2, even though it has index 0 */}
-                <Td>{error.id + 2}</Td>
-                <Td>
-                  {error.param}{' '}
-                  {error.message ===
-                  BulkLetterValidationResultErrorMessage.INVALID_ATTRIBUTE
-                    ? 'is not a valid attribute'
-                    : 'is missing'}
-                </Td>
-              </Tr>
-            )
-          })}
+          {errorTableRows}
         </Table>
       </TableContainer>
     </Box>
