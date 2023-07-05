0a2945328881f78be768e7540f4ec8d2913f8e72
PikkaPikkachu <prakriti_bansal@yahoo.com>
2023-07-05 14:49:37 +0800

    fix: add template -> request template button

--- frontend/src/features/dashboard/TemplatesPage.tsx
+++ frontend/src/features/dashboard/TemplatesPage.tsx
@@ -1,9 +1,6 @@
 import { Box, VStack } from '@chakra-ui/react'
 import { Button } from '@opengovsg/design-system-react'
-import { useNavigate } from 'react-router-dom'
-
-import { ReactComponent as PlusCircle } from '~/assets/PlusCircle.svg'
-import { routes } from '~constants/routes'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { TemplatesBody } from './components/TemplatesBody'
 
@@ -12,15 +9,12 @@ export const TemplatesPage = (): JSX.Element => {
 
   return (
     <VStack alignItems="left" spacing="0px">
-      <VStack pt={2.5} align={'center'}>
-        <Button
-          marginLeft={8}
-          alignSelf="start"
-          leftIcon={<PlusCircle />}
-          onClick={() => navigate(`/admin/${routes.admin.create}`)}
-        >
-          Add New
-        </Button>
+      <VStack pt={2.5} align={'start'}>
+        <Link to="https://go.gov.sg/letters-beta" target="_blank">
+          <Button marginLeft={8} alignSelf="start">
+            Request New Template
+          </Button>
+        </Link>
         <Box>
           <TemplatesBody />
         </Box>
