0e4ff4a7b396fdd897997b60fa4a892cbc0a62b6
PikkaPikkachu <prakriti_bansal@yahoo.com>
2023-07-11 23:39:53 +0800

    chore: add logo to header

--- frontend/src/features/templates/components/TemplateHeader.tsx
+++ frontend/src/features/templates/components/TemplateHeader.tsx
@@ -1,16 +1,16 @@
-import { Box, Flex, HStack, Icon, Text } from '@chakra-ui/react'
+import { Box, Flex, HStack, Image, Text } from '@chakra-ui/react'
 import {
   Step,
   StepIcon,
   StepIndicator,
   StepNumber,
   Stepper,
-  StepSeparator,
   StepStatus,
 } from '@chakra-ui/stepper'
 import { Button } from '@opengovsg/design-system-react'
-import { BiLeftArrowAlt } from 'react-icons/bi'
 import { Link as RouterLink } from 'react-router-dom'
+
+import LogoSvg from '~/assets/Logo.svg'
 
 export interface TemplateHeaderProps {
   activeStep: number
@@ -26,7 +26,7 @@ export const TemplateHeader = ({
       position="static"
       pos="relative"
       flexDir="row"
-      p={5}
+      p={2}
       justifyContent="space-between"
       borderBottom="1px"
       borderBottomColor="base.divider.medium"
@@ -36,7 +36,10 @@ export const TemplateHeader = ({
       <HStack spacing={4}>
         <HStack spacing={4} fontSize={'14px'}>
           <RouterLink to={'/admin/templates'}>
-            <Button variant="link" leftIcon={<BiLeftArrowAlt />}>
+            <Button
+              variant="link"
+              leftIcon={<Image src={LogoSvg} h={'80%'} w={'80%'} pl={'20px'} />}
+            >
               All templates
             </Button>
           </RouterLink>
@@ -45,7 +48,13 @@ export const TemplateHeader = ({
         </HStack>
         {/* Fix styling of the stepper, shouldn't be using paddingLeft technically */}
         <Box paddingLeft={'225px'}>
-          <Stepper index={activeStep} gap="20" color="grey.200">
+          <Stepper
+            index={activeStep}
+            color="grey.200"
+            gap="10"
+            fontSize={'12'}
+            size={'sm'}
+          >
             {steps.map((step, index) => (
               <Step key={index}>
                 <StepIndicator>
@@ -56,7 +65,7 @@ export const TemplateHeader = ({
                   />
                 </StepIndicator>
 
-                <Text textStyle="caption-3" fontSize={'14px'} flexShrink={'0'}>
+                <Text textStyle="caption-3" flexShrink={'0'}>
                   {step}
                 </Text>
               </Step>
